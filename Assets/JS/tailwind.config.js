b1864e2679e10ac26e096dd5c4c8f8f850168759
fire8327 <fire83274@gmail.com>
2023-01-13 22:30:02 +0300

    create header

--- Assets/JS/tailwind.config.js
+++ Assets/JS/tailwind.config.js
@@ -2,7 +2,17 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
+        'mainred':'#a31d1d',
         },
+    },
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '10px',
+        '2xl': '30px',
       },
+    },
   },
 };

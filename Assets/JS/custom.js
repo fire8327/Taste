edb4c8ffe8fec2b27c57dd34e41793fafff106f3
fire8327 <fire83274@gmail.com>
2023-01-17 20:19:41 +0300

    fix header

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -8,11 +8,49 @@ if (prev.classList.contains("swiper-button-disabled") && next.classList.contains
 
 /* navigation */
 /*jshint esnext: true */
-const toggler = document.getElementById("toggler");
+const toggler1 = document.getElementById("toggler1");
+const toggler2 = document.getElementById("toggler2");
+const close = document.getElementById("close");
 const mNav = document.getElementById("mobile__nav");
 const body = document.getElementsByTagName("body")[0];
 
-toggler.addEventListener("click", () => {
+toggler1.addEventListener("click", () => {
   mNav.classList.toggle("opacity-0");
   body.classList.toggle("overflow-x-hidden");
 });
+toggler2.addEventListener("click", () => {
+  mNav.classList.toggle("opacity-0");
+  body.classList.toggle("overflow-x-hidden");
+});
+close.addEventListener("click", () => {
+  mNav.classList.toggle("opacity-0");
+  body.classList.toggle("overflow-x-hidden");
+});
+
+/* search */
+const search = document.getElementById("search");
+const close_search = document.getElementById("close_search");
+const search_block = document.getElementById("search_block");
+
+search.addEventListener("click", () => {
+  search_block.classList.remove("opacity-0");
+  search_block.classList.remove("-translate-y-full");
+});
+close_search.addEventListener("click", () => {
+  search_block.classList.add("opacity-0");
+  search_block.classList.add("-translate-y-full");
+});
+
+/* back to top */
+const toTopButton = document.getElementById("to-top-button");
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        toTopButton.classList.remove("hidden");
+    } else {
+        toTopButton.classList.add("hidden");
+    }
+}
+
+function goToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}

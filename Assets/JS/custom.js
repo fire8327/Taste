135ba02add756f07c8e7348e07f838f9d3a72c4c
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-01-17 11:31:13 +0000

    fix

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -5,3 +5,14 @@ let btn = document.querySelector(".buttons");
 if (prev.classList.contains("swiper-button-disabled") && next.classList.contains("swiper-button-lock")) {
     btn.classList.add("hidden");
 }
+
+/* navigation */
+/*jshint esnext: true */
+const toggler = document.getElementById("toggler");
+const mNav = document.getElementById("mobile__nav");
+const body = document.getElementsByTagName("body")[0];
+
+toggler.addEventListener("click", () => {
+  mNav.classList.toggle("opacity-0");
+  body.classList.toggle("overflow-x-hidden");
+});
